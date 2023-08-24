beddafc65a26a2260c6100020ba8e342bdd961ef
khadyka <nk.ka3@isepdiamniadio.edu.sn>
2023-08-24 14:45:47 +0000

    deu

---
+++ gitdeux/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+import TipCalculator from './TipCalculator'; // Assurez-vous que le chemin d'accès est correct
+
+function App() {
+  return (
+    <div className="App">
+      <TipCalculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ gitdeux/src/TipCalculator.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+function TipCalculator() {
+  const [billAmount, setBillAmount] = useState('');
+  const [tipPercentage, setTipPercentage] = useState(15);
+  const [tipAmount, setTipAmount] = useState(0);
+
+  const calculateTip = () => {
+    const bill = parseFloat(billAmount);
+    const tip = (bill * tipPercentage) / 100;
+    setTipAmount(tip);
+  };
+
+  return (
+    <div>
+      <h1>Calculatrice de Pourboire</h1>
+      <label>
+        Montant de la facture: 
+        <input
+          type="number"
+          value={billAmount}
+          onChange={(e) => setBillAmount(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Pourcentage de pourboire: 
+        <input
+          type="number"
+          value={tipPercentage}
+          onChange={(e) => setTipPercentage(e.target.value)}
+        />
+        %
+      </label>
+      <br />
+      <button onClick={calculateTip}>Calculer le pourboire</button>
+      <p>Montant du pourboire: {tipAmount.toFixed(2)}</p>
+    </div>
+  );
+}
+
+export default TipCalculator;
